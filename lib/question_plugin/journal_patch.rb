3e95e95decd3f734faef52daa31b5851132b765e
pdf <github@obfusc8.org>
2009-11-13 20:16:59 +1100

    add module QuestionPlugin for previous commit

--- lib/question_plugin/journal_patch.rb
+++ lib/question_plugin/journal_patch.rb
@@ -1,22 +1,24 @@
-module JournalPatch
-  def self.included(base) # :nodoc:
-    base.extend(ClassMethods)
-
-    base.send(:include, InstanceMethods)
-
-    base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
-      has_one :question
+module QuestionPlugin
+  module JournalPatch
+    def self.included(base) # :nodoc:
+      base.extend(ClassMethods)
+  
+      base.send(:include, InstanceMethods)
+  
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+        has_one :question
+      end
+  
     end
-
-  end
-  
-  module ClassMethods
-  end
-  
-  module InstanceMethods
-    def question_assigned_to
-      # TODO: pull out the assigned user on edits
+    
+    module ClassMethods
+    end
+    
+    module InstanceMethods
+      def question_assigned_to
+        # TODO: pull out the assigned user on edits
+      end
     end
   end
 end
