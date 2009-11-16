1c98c5ff488a5bb21c59e8dd33ae75f60e37d354
pdf <github@obfusc8.org>
2009-11-16 18:37:46 +1100

    Tidy mailer templates/subjects, and use render_multipart to fix b0rken rendering

--- app/models/question_mailer.rb
+++ app/models/question_mailer.rb
@@ -1,7 +1,7 @@
 class QuestionMailer < Mailer
   def asked_question(journal)
     question = journal.question
-    subject "[Question ##{question.issue.id}] #{question.issue.subject}"
+    subject "[Question - #{question.issue.tracker.name} ##{question.issue.id}] #{question.issue.subject}"
     recipients question.assigned_to.mail unless question.assigned_to.nil?
     @from  = "#{question.author.name} (Redmine) <#{Setting.mail_from}>" unless question.author.nil?
 
@@ -12,14 +12,13 @@ class QuestionMailer < Mailer
       :issue_url => url_for(:controller => 'issues', :action => 'show', :id => question.issue)
     })
 
-    part :content_type => "text/plain", :body => render_message("asked_question.erb", body)
-    part :content_type => "text/html", :body => render_message("asked_question.text.html.rhtml", body)
+    render_multipart('asked_question', body)
 
     RAILS_DEFAULT_LOGGER.debug 'Sending QuestionMailer#asked_question'
   end
   
   def answered_question(question, closing_journal)
-    subject "[Answered] #{question.issue.subject} ##{question.issue.id}"
+    subject "[Answered - #{question.issue.tracker.name} ##{question.issue.id}] #{question.issue.subject}"
     recipients question.author.mail unless question.author.nil?
     @from = "#{question.assigned_to.name} (Redmine) <#{Setting.mail_from}>" unless question.assigned_to.nil?
 
@@ -30,8 +29,7 @@ class QuestionMailer < Mailer
       :issue_url => url_for(:controller => 'issues', :action => 'show', :id => question.issue)
     })
 
-    part :content_type => "text/plain", :body => render_message("answered_question.erb", body)
-    part :content_type => "text/html", :body => render_message("answered_question.text.html.rhtml", body)
+    render_multipart('answered_question', body)
 
     RAILS_DEFAULT_LOGGER.debug 'Sending QuestionMailer#answered_question'
   end
